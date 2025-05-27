f24cfbe5b964d6251d9274385154dcd01fb3d526
Greg Carter <greg9504@gmail.com>
2025-05-27 01:16:38 -0400

    pass runtime to ethernetip plugin, fix up passed value to explicit messages, use JSON.stringify on error messages

--- server/runtime/devices/device.js
+++ server/runtime/devices/device.js
@@ -125,7 +125,7 @@ function Device(data, runtime) {
         if (!GenericEthernetIPclient) {
             return null;
         }
-        comm = GenericEthernetIPclient.create(data, logger, events, manager); 
+        comm = GenericEthernetIPclient.create(data, logger, events, manager, runtime); 
     } 
     // else if (data.type === DeviceEnum.Template) {
     //     if (!TEMPLATEclient) {
